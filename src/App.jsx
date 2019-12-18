0074023350dae2188a25e6997d4edb9a34eff10c
Diego <dflores7237@gmail.com>
2019-12-17 16:55:17 -0800

    Created a functional version of the TaskCreator app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+     
+    this.state = {}
+  }
 
+  createNewTask(task,priority){
+    this.setState({[task]: priority});
+    console.log(Object.keys(this.state).length, 'state length');
+    console.log(this.state, 'current state')
+  }
   render() {
     return (
       <div className='container'>
@@ -9,18 +19,39 @@ class App extends Component {
         <hr/>
         <div className='container-fluid'>
           <div className='row'>
-            <TaskCreator />
+            <TaskCreator newTask={this.createNewTask.bind(this)}/>
             <TaskViewer />
           </div>
         </div>
-
       </div>
     );
   }
 }
 
 class TaskCreator extends Component {
+  constructor(props) {
+    super(props);
+  }
+  // the onClickHandler function will be creating a new task using the user input data
+  // and storing the task in the App component state. Each task will exist as key:value 
+  //pair in the state. The key will be equal to the task text and the value property is 
+  // described above the taskProperty variable below.
+  onClickHandler(){
+    const taskKey = document.getElementById('setTaskText').value;
+    const taskPriority = document.getElementById('setTaskPriority').value;
+    // The taskProperty variable will be used to set the task priority ([0] position) and current
+    // state of the view window in the task editior ([1] postion). By default the property will
+    // be created with a view window value of "0". 
+    const taskProperty = [taskPriority,0];
+    if (taskKey.length > 0) {
+      this.props.newTask(taskKey, taskProperty);
+    } else {
+      alert('Please enter some text to create this task!')
+    };
+    document.getElementById('setTaskText').value = '';
+    document.getElementById('setTaskPriority').value = 1;
 
+  }
   render() {
     return (
       <div className='container-fluid col-lg-4'>
@@ -40,10 +71,9 @@ class TaskCreator extends Component {
             </div>
           </div>
           <div className='panel-footer'>
-            <button type='button' className='btn btn-success btn-block'>Add</button>
+            <button type='button' className='btn btn-success btn-block' onClick={this.onClickHandler.bind(this)}>Add</button>
           </div>
         </div>
-
       </div>
     );
   }
@@ -129,5 +159,7 @@ function TaskEditWindow(props) {
     </div>
     );
   }
+
+  const tasks = {};
 
 export default App;
